c4e077eb59294b25f0eb263383d6efedb930c841
Fikky Wirita <Fikky Wirita@FikkyWirita>
2018-03-22 15:32:51 +0700

    ganti index include sama isi main controller

--- src/main/java/com/miniproject/kel2/controller/Maincontroller.java
+++ src/main/java/com/miniproject/kel2/controller/Maincontroller.java
@@ -1,5 +1,14 @@
 package com.miniproject.kel2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
 public class Maincontroller {
 
+	@RequestMapping
+	public String index() {
+		return "index";
+	}
 }
