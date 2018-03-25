0ddae30535569630d9469bb237b5073d11e1ea34
FUJITSU <FUJITSU@DESKTOP-99L36JC>
2018-03-26 00:33:24 +0700

    tambah empOutlet

---
+++ src/main/java/com/miniproject/kel2/model/EmpOutlet.java
@@ -0,0 +1,52 @@
+package com.miniproject.kel2.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "outlet_entity")
+public class EmpOutlet {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	
+	
+	@ManyToOne
+	private Employee employee;
+	
+	@ManyToOne
+	private Outlet outlet;
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Outlet getOutlet() {
+		return outlet;
+	}
+
+	public void setOutlet(Outlet outlet) {
+		this.outlet = outlet;
+	}
+	
+	
+	
+}
